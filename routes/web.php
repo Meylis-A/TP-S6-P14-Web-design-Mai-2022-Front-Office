d0ba7017bc6fa46b8600ee98dda03341a0b55c90
Meylis-A <meylisalain@gmail.com>
2023-05-07 21:57:11 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ArticlesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/',function(){
+//     return view('articles.index');
+// });
+
+Route::get('/', [ArticlesController::class, 'index'])->name('home');
+Route::get('/softland/blog-post/{url}', [ArticlesController::class, 'blog_post'])->name('blog-post');
+Route::get('/softland/create', [ArticlesController::class, 'create'])->name('articles.create');
+Route::post('/softland', [ArticlesController::class, 'store'])->name('articles.store');
+Route::get('/softland/{url}/{article}', [ArticlesController::class, 'show'])->name('show');
+Route::get('/softland/{article}', [ArticlesController::class, 'edit'])->name('articles.edit');
+Route::put('/softland/{article}', [ArticlesController::class, 'update'])->name('articles.update');
+Route::delete('/softland/{article}', [ArticlesController::class, 'destroy'])->name('articles.destroy');

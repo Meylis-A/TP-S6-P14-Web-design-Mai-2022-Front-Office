ab67dd4eb4448a68aef88fa227a469f5ff150584
Meylis-A <meylisalain@gmail.com>
2023-05-08 23:21:03 +0300

    UPdate

---
+++ app/Http/Middleware/GzipMiddleware.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class GzipMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        if ($response->isSuccessful() && $response->isText()) {
+            $content = gzencode($response->getContent(), 9);
+
+            $response->header('Content-Encoding', 'gzip');
+            $response->header('Content-Length', strlen($content));
+            $response->setContent($content);
+        }
+
+        return $response;
+    }
+}
